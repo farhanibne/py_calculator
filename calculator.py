29e5b08c4709cb77faf710a835c766c4d935b632
farhanibne <84982846+farhanibne@users.noreply.github.com>
2021-08-25 19:12:32 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,29 @@
+#function
+
+a = input("enter first number: ") 
+b = input("enter second number: ")
+
+a = int(a)
+b = int(b)
+
+#what to do
+
+add = (a+b)
+sub = (a-b)
+mul = (a*b)
+div = (a/b)
+avg = (a+b)/2
+mod = (a%b)
+big = (a>b)
+sma = (a<b)
+
+#printing
+
+print('addision: ', add)
+print('substraction: ', sub)
+print('multiplication: ', mul)
+print('division: ', div)
+print('average:', avg)
+print('modulas: ', mod)
+print('first one is bigger:', big)
+print('secong one is bigger:', sma)
